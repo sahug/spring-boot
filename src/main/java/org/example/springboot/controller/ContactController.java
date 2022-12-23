c9351d77b9a8cc116ac1d1682a7a70a12b5b3c4b
Gajendra Sahu <gajendraks1988@gmail.com>
2022-12-23 11:58:42 -0600

    Configure About and Contact Page
    Configure About and Contact Page

---
+++ src/main/java/org/example/springboot/controller/ContactController.java
@@ -0,0 +1,14 @@
+package org.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    @RequestMapping("/contact")
+    public String displayContactPage() {
+        return "contact.html";
+    }
+
+}
