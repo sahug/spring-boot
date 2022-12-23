6fd41933279e5b3e191786624bf83a505c00d2e1
Gajendra Sahu <gajendraks1988@gmail.com>
2022-12-23 12:31:47 -0600

    Using POJO to Submit Information
    Using POJO to Submit Information

--- src/main/java/org/example/springboot/controller/ContactController.java
+++ src/main/java/org/example/springboot/controller/ContactController.java
@@ -1,7 +1,10 @@
 package org.example.springboot.controller;
 
+import org.example.springboot.model.Contact;
+import org.example.springboot.service.ContactService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,11 +18,19 @@ public class ContactController {
 
     private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
     @RequestMapping("/contact")
     public String displayContactPage() {
         return "contact.html";
     }
 
+    /*
     @RequestMapping(value = "/saveMsg", method = POST)
     public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
                                     @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
@@ -30,5 +41,13 @@ public class ContactController {
         log.info("Message : " + message);
         return new ModelAndView("redirect:/contact");
     }
+    */
+
+    @RequestMapping(value = "/saveMsg", method = POST)
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
+        return new ModelAndView("redirect:/contact");
+    }
+    
 
 }
