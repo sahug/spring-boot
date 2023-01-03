71cfdec4c3d600d721cfb4b419c733d898f96fe1
Gajendra Sahu <gajendraks1988@gmail.com>
2023-01-03 12:48:05 -0600

    PermitAll And DenyAll using Spring Security
    PermitAll And DenyAll using Spring Security

---
+++ src/main/java/org/example/springboot/config/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package org.example.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * permitAll() allows full/public access to a specific or all resource/path inside a web application.
+     * Used to allow public access to public access to public APIs, paths, CSS, images, JS files etc.
+     * */
+    /*
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .anyRequest().permitAll()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+    */
+
+    /**
+     * denyAll() denies full/public access to a specific or all resource/path inside a web application
+     * Used to retire a specific API temporarily without removing code
+     * */
+    /*
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .anyRequest().denyAll()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+    */
+
+    /**
+     * From Spring Security 5.7, the WebSecurityConfigurerAdapter is deprecated to encourage users
+     * to move towards a component-based security configuration. It is recommended to create a bean
+     * of type SecurityFilterChain for security related configurations.
+     * @param httpSecurity
+     * @return SecurityFilterChain
+     * @throws Exception
+     */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        /** Permit All Requests inside the Web Application */
+        /*
+        httpSecurity.authorizeRequests().anyRequest().permitAll()
+                        .and().formLogin()
+                        .and().httpBasic();
+        */
+
+        /** Deny All Requests inside the Web Application */
+        /*
+        httpSecurity.authorizeRequests().anyRequest().denyAll()
+                .and().formLogin()
+                .and().httpBasic();
+        */
+        return httpSecurity.build();
+
+    }
+}
